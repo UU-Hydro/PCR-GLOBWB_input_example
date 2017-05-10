1dd136f2c4f288fc8bb2d2587b7f4c8526fc15ca
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-05-10 13:35:14 +0200

    test again on edwinhs

--- ini_and_batch_files_for_pcrglobwb_course/RhineMeuse30min_using_input_example/run.sh
+++ ini_and_batch_files_for_pcrglobwb_course/RhineMeuse30min_using_input_example/run.sh
@@ -3,4 +3,4 @@
 # $ python <directory_where_you_store_your_pcrglobwb_model_file>/deterministic_runner.py <your_ini_file> 
 
 # an example:
-python /home/edwinwri/github/UU-Hydro/PCR-GLOBWB_model/model/deterministic_runner.py setup_natural_test.ini
+python /home/edwinhs/github/UU-Hydro/PCR-GLOBWB_model/model/deterministic_runner.py setup_natural_test.ini
