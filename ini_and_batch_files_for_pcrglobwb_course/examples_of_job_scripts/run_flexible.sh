ff58afa3e731fa948015600cef1e8d5845543dbc
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2017-05-11 18:51:13 +0200

    modify job files

--- ini_and_batch_files_for_pcrglobwb_course/examples_of_job_scripts/run_flexible.sh
+++ ini_and_batch_files_for_pcrglobwb_course/examples_of_job_scripts/run_flexible.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -t 80:00:00
 #SBATCH -p normal
+#SBATCH -t 80:00:00
+
+# -t : Wall clock time (example: 80:00:00 for 80 hours).
+# For every PCR-GLOBWB job, number of nodes that will be reserved should be 1 (-N 1) and a normal node type is recommended (-p normal).
+# Please note that there is still no MPI implementation for PCRaster or PCR-GLOBWB.
 
 while countfiles=20
 do 
