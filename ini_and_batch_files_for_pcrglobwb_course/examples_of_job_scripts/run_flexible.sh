0e3a6110ec910089f4d477c7e98da152c74a7764
Edwin Sutanudjaja <edwinvua@cartesius.surfsara.nl>
2018-02-05 22:18:56 +0100

    Fixing run_flexible.sh

--- ini_and_batch_files_for_pcrglobwb_course/examples_of_job_scripts/run_flexible.sh
+++ ini_and_batch_files_for_pcrglobwb_course/examples_of_job_scripts/run_flexible.sh
@@ -2,10 +2,12 @@
 #SBATCH -N 1
 #SBATCH -p normal
 #SBATCH -t 0:30:00
-#~ #SBATCH --reservation=pcrglobwb 
+#SBATCH --reservation=pcr_globwb 
 
-# --reservation=pcrglobwb is needed for PCR-GLOBWB training on 15-19 May 2017
+# --reservation=pcr_globwb is needed during the PCR-GLOBWB course
+
 # -t : Wall clock time (example: 80:00:00 for 80 hours). Please calculate it, maxiumum until the end of the course. 
+
 # For every PCR-GLOBWB job, number of nodes that will be reserved should be 1 (-N 1) and a normal node type is recommended (-p normal).
 # Please note that there is still no MPI implementation for PCRaster (and PCR-GLOBWB).
 
